c558a690413ccdc0290f0e3d7e37fcf3900c6bf2
Mobin Skaria <mobin0@berkeley.edu>
2022-06-20 13:00:49 -0700

    First Commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videogames = new List<string>() {
+            "Ratchet and Clank",
+            "Jak and Daxter",
+            "Runescape"
+            };
+            IEnumerable<string> results = videogames.OrderBy(x => x.Length);
+
+            foreach (string i in results) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
